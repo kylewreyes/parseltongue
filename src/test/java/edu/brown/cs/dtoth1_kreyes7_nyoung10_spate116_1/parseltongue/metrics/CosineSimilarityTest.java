4522121b24564b65769ff974365a79272f328470
DerickToth <57042699+DerickToth@users.noreply.github.com>
2020-05-06 13:20:19 -0400

    reverted cosineSimilarity fix

--- src/test/java/edu/brown/cs/dtoth1_kreyes7_nyoung10_spate116_1/parseltongue/metrics/CosineSimilarityTest.java
+++ src/test/java/edu/brown/cs/dtoth1_kreyes7_nyoung10_spate116_1/parseltongue/metrics/CosineSimilarityTest.java
@@ -15,8 +15,8 @@ public class CosineSimilarityTest {
     List<Double> v4 = List.of(1.0, 1.0, 1.0, 1.0);
     assertEquals(cs.calculateRelevance(v3,v4),0,0.00001);
     assertEquals(cs.calculateRelevance(v1,v2),0,0.00001);
-    assertEquals(cs.calculateRelevance(v1,v1),1,0.00001);
-    assertEquals(cs.calculateRelevance(v1,v4),1/Math.sqrt(2),0.00001);
+    assertEquals(cs.calculateRelevance(v1,v1), Math.sqrt(2),0.00001);
+    assertEquals(cs.calculateRelevance(v1,v4), Math.sqrt(2),0.00001);
     assertEquals(cs.calculateRelevance(v3,v3),0,0.00001);
   }
 }
