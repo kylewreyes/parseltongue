7f4c7b6c78c42948b14d53e7ed959c55e01476e2
Kyle <kyle_reyes@brown.edu>
2020-05-06 16:04:56 -0400

    Testing

--- src/test/java/edu/brown/cs/dtoth1_kreyes7_nyoung10_spate116_1/parseltongue/parser/SnippetTest.java
+++ src/test/java/edu/brown/cs/dtoth1_kreyes7_nyoung10_spate116_1/parseltongue/parser/SnippetTest.java
@@ -2,7 +2,10 @@ package edu.brown.cs.dtoth1_kreyes7_nyoung10_spate116_1.parseltongue.parser;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -24,19 +27,55 @@ public class SnippetTest {
 
   @Test
   public void parseTextTest() {
+    File f = new File("data/references_on_first_page.pdf");
+    try (PDFParser parser = new PDFParser(f)) {
+      List<String> pages = new ArrayList<>();
+      for (int i = 1; i <= parser.getPageCount(); i++) {
+        pages.add(parser.getTextFromPage(i));
+      }
+      List<String> expectedStrings = new ArrayList<>();
+      expectedStrings.add("URUGUAY: THE RISE AND FALL OF A WELFARE STATE SEEN AGAINST A " +
+          "BACKGROUND" + System.lineSeparator() +
+          "OF DEPENDENCY THEORY" + System.lineSeparator() +
+          "Author(s): J.M.G. Kleinpenning" + System.lineSeparator() +
+          "Source: Revista Geográfica, No. 93 (ENERO-JUNIO 1981), pp. 101-117" + System.lineSeparator() +
+          "Published by: Pan American Institute of Geography and History" + System.lineSeparator() +
+          "Stable URL: https://www.jstor.org/stable/40993122" + System.lineSeparator() +
+          "Accessed: 21-04-2020 16:28 UTC" + System.lineSeparator() +
+          "REFERENCES" + System.lineSeparator() +
+          "Linked references are available on JSTOR for this article:" + System.lineSeparator() +
+          "https://www.jstor.org/stable/40993122?seq=1&cid=pdf-reference#references_tab_contents" +
+          "" + System.lineSeparator() +
+          "You may need to log in to JSTOR to access the linked references.");
+      expectedStrings.add("JSTOR is a not-for-profit service that helps scholars, researchers, " +
+          "and students discover, use, and build upon a wide" + System.lineSeparator() +
+          "range of content in a trusted digital archive. We use information technology and tools" +
+          " to increase productivity and" + System.lineSeparator() +
+          "facilitate new forms of scholarship. For more information about JSTOR, please contact " +
+          "support@jstor.org.");
+      expectedStrings.add("Your use of the JSTOR archive indicates your acceptance of the Terms &" +
+          " Conditions of Use, available at" + System.lineSeparator() +
+          "https://about.jstor.org/terms" + System.lineSeparator() +
+          "Pan American Institute of Geography and History is collaborating with JSTOR to " +
+          "digitize," + System.lineSeparator() +
+          "preserve and extend access to Revista Geográfica" + System.lineSeparator());
+
+      List<String> snippetStrings = new ArrayList<>();
+      for (Snippet s : Snippet.parseText(pages, f.getName())) {
+        snippetStrings.add(s.getOriginalText());
+      }
+      assertEquals(expectedStrings, snippetStrings);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
     try (PDFParser parser = new PDFParser("data/UruguayWithFace.pdf")) {
 
     } catch (IOException e) {
       e.printStackTrace();
     }
-    /*
-  Test for separation by paragraphs
-  Test for margins (headers and footers)
-  Test for multiple pages
-  Test for removing bibliographies
-  - Lines that contain (Capital letter): references, works cited, bibliography, references cited, acknowledgements
-  - Word by itself OR First word in the line but followed by period or colon
-   */
+
   }
 
 }
